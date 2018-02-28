a0e0c04aa92ff5438b68e02c243ab5b95e3e7f31
Felix Natter <fnatter@gmx.net>
2018-02-28 18:04:48 +0100

    remove cruft from version control

--- NW-cmake/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/felix/workspaces/cpptest/NW-cmake
-# Build directory: /home/felix/workspaces/cpptest/NW-cmake
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AlignmentRuns "Alignment" "AC" "ABC" "1" "-1" "-1")
